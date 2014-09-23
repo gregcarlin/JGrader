c20aa18529f423dc4197924a2420955198fe2d53
Brian <brian@convolve.com>
2014-09-23 17:10:10 -0400

    Messed around with form, should be working

--- JGrader/app/controllers/Application.java
+++ JGrader/app/controllers/Application.java
@@ -3,15 +3,31 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import play.data.*;
+import static play.data.Form.*;
 
 import views.html.*;
 
-public static class Login{
-	public String email;
-	public String password;
-}
+
 
 public class Application extends Controller {
+
+	// Stores Login Information
+	public static class Login{
+		public String email;
+		public String password;
+
+		// Sends back error string if something is wrong
+		public String validate() {
+		    return null;
+		}
+	}	
+
+	// Reads Login form and see if ok
+	public static Result authenticate() {
+    	Form<Login> loginForm = form(Login.class).bindFromRequest();
+    	return ok();
+	}
+
 
     public static Result index() {
         return ok(login.render(form(Login.class)));
@@ -24,4 +40,6 @@ public class Application extends Controller {
     public static Result teacher() {
     	return ok(teacher.render());
     }
+
+    
 }
