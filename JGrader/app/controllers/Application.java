de7750f3d372877aceb48710291b5445b5c2be19
Brian <brian@convolve.com>
2014-09-23 09:27:58 -0400

    Started Login classes

--- JGrader/app/controllers/Application.java
+++ JGrader/app/controllers/Application.java
@@ -2,13 +2,19 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
 
 import views.html.*;
+
+public static class Login{
+	public String email;
+	public String password;
+}
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(login.render());
+        return ok(login.render(form(Login.class)));
     }
 
     public static Result student() {
@@ -18,6 +24,4 @@ public class Application extends Controller {
     public static Result teacher() {
     	return ok(teacher.render());
     }
-
-
 }
