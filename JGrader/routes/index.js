b8e3f9c4897b0b479eb640b3ecf3754cf50849b5
Greg Carlin <gregthegeek@optonline.net>
2014-11-28 17:14:45 -0500

    Everything for login but the actual login

--- JGrader/routes/index.js
+++ JGrader/routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+var creds   = require('./credentials');
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : creds.mysql_host,
+  database : creds.mysql_db,
+  user     : creds.mysql_user,
+  password : creds.mysql_pass
+});
+connection.connect(); // we should probably close this at some point [connection.end()]
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -10,8 +20,15 @@ router.post('/', function(req, res) {
   var email = req.param('email');
   var pass = req.param('password');
   if(email && pass) {
-    // todo authenticate and redirect
-    
+    connection.query('SELECT * FROM `students` WHERE `user` = ? AND `pass` = ?', [email, pass], function(err, rows) {
+      if(err) throw err; // todo better error handling?
+
+      if(rows.length > 0) {
+        // todo login
+      } else {
+        res.render('index', { title: 'Express', error: 'Incorrect username or password.', email: email });
+      }
+    });
   } else {
     res.render('index', { title: 'Express', error: 'All fields are required.', email: email });
   }
