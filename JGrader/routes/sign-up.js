005f9db2df0bbccd3cc9669757438ae885d3e12e
Greg Carlin <gregthegeek@optonline.net>
2014-11-29 16:01:57 -0500

    Added support for teaching assistants
    Not tested

--- JGrader/routes/sign-up.js
+++ JGrader/routes/sign-up.js
@@ -18,49 +18,52 @@ router.get('/', function(req, res) {
   res.render('sign-up', {});
 });
 
+// checks if a user exists in a given database. calls finish() iff user doesn't exist.
+var exists = function(fname, lname, email, role, db, res, finish) {
+  connection.query("SELECT `id` FROM `" + db + "` WHERE `user` = ?", [email], function(err, rows) {
+    if(err) {
+      res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
+    } else if(rows.length > 0) {
+      res.render('sign-up', { error: 'An account with that email already exists.', fname: fname, lname: lname, email: email, role: role });
+    } else {
+      finish();
+    }
+  });
+}
+
 router.post('/', function(req, res) {
   var fname = req.param('fname');
   var lname = req.param('lname');
   var email = req.param('email');
   var pass  = req.param('password');
   var role  = req.param('role');
-  if(fname && lname && email && pass && role && (role == 'student' || role == 'teacher')) {
-    connection.query("SELECT `id` FROM `students` WHERE `user` = ?", [email], function(err, rows) {
-      if(err) {
-        res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
-      } else if(rows.length > 0) {
-        res.render('sign-up', { error: 'An account with that email already exists.', fname: fname, lname: lname, email: email, role: role });
-      } else {
-        connection.query("SELECT `id` FROM `teachers` WHERE `user` = ?", [email], function(err, rows) {
-          if(err) {
-            res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
-          } else if(rows.length > 0) {
-            res.render('sign-up', { error: 'An account with that email already exists.', fname: fname, lname: lname, email: email, role: role });
-          } else {
-            connection.query("INSERT INTO `" + role + "s` VALUES(NULL, ?, AES_ENCRYPT(?, '" + creds.aes_key + "'), ?, ?)", [email, pass, fname, lname], function(err, rows) {
-              if(err) {
-                res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
-              } else {
-                connection.query("SELECT `id` FROM `" + role + "s` WHERE `user` = ?", [email], function(err, rows) {
-                  if(err || rows.length <= 0) {
-                    res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
-                  } else {
-                    var hash = crypto.randomBytes(20).toString('hex'); // http://stackoverflow.com/a/14869745/720889
-                    res.cookie('hash', hash);
-                    connection.query('INSERT INTO `sessions-' + role + 's` VALUES(?, ?)', [rows[0].id, hash], function(err, rows) {
-                      if(err) {
-                        res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
-                      } else {
-                        res.redirect('/' + role);
-                      }
-                    });
-                  }
-                });
-              }
-            });
-          }
+  if(fname && lname && email && pass && role && (role == 'student' || role == 'teacher' || role == 'assistant')) {
+    exists(fname, lname, email, role, 'students', res, function() {
+      exists(fname, lname, email, role, 'teachers', res, function() {
+        exists(fname, lname, email, role, 'assistants', res, function() {
+          connection.query("INSERT INTO `" + role + "s` VALUES(NULL, ?, AES_ENCRYPT(?, '" + creds.aes_key + "'), ?, ?)", [email, pass, fname, lname], function(err, rows) {
+            if(err) {
+              res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
+            } else {
+              connection.query("SELECT `id` FROM `" + role + "s` WHERE `user` = ?", [email], function(err, rows) {
+                if(err || rows.length <= 0) {
+                  res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
+                } else {
+                  var hash = crypto.randomBytes(20).toString('hex'); // http://stackoverflow.com/a/14869745/720889
+                  res.cookie('hash', hash);
+                  connection.query('INSERT INTO `sessions-' + role + 's` VALUES(?, ?)', [rows[0].id, hash], function(err, rows) {
+                    if(err) {
+                      res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
+                    } else {
+                      res.redirect('/' + role);
+                    }
+                  });
+                }
+              });
+            }
+          });
         });
-      }
+      });
     });
   } else {
     res.render('sign-up', { error: 'All fields are required.', fname: fname, lname: lname, email: email, role: role });
