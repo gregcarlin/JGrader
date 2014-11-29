d87187e80de935659596caf4cc64c807c7d93f31
Brian <brian@convolve.com>
2014-11-28 20:57:37 -0500

    setting up routes for teachers and students

--- JGrader/app.js
+++ JGrader/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var teacher = require('./routes/teacher');
+var student = require('./routes/student');
 
 var app = express();
 
@@ -23,7 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-//app.use('/teacher', teacher);
+app.use('/teacher', teacher);
+app.use('/student', student);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ JGrader/routes/teacher.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router  = express.Router();
+var creds   = require('./credentials');
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : creds.mysql_host,
+  port     : creds.mysql_port,
+  database : creds.mysql_db,
+  user     : creds.mysql_user,
+  password : creds.mysql_pass
+});
+connection.connect(); // we should probably close this at some point [connection.end()]
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
