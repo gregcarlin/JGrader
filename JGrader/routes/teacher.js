ef91b43365a7713ab00aeded57788a803eebee81
Brian <brian@convolve.com>
2014-12-02 11:45:03 -0500

    working on teacher submissions

--- JGrader/routes/teacher.js
+++ JGrader/routes/teacher.js
@@ -17,7 +17,26 @@ router.get('/', function(req, res) {
   res.render('teacher/teacherIndex', { title: 'Express' });
 });
 
+router.get('/createClass', function(req, res){
+  res.render('teacher/createClass', { title: 'Express' });
+});
+router.post('/submitClass', function(req,res) {
+
+});
+
 router.post('/submitAssignment', function(req,res) {
 
 });
 module.exports = router;
+
+// var exists = function(class, id, res, finish) {
+//   connection.query("SELECT `id` FROM `sections` WHERE `name` = ?", [class], function(err, rows) {
+//     if(err) {
+//       res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
+//     } else if(rows.length > 0) {
+//       res.render('sign-up', { error: 'An account with that email already exists.', fname: fname, lname: lname, email: email, role: role });
+//     } else {
+//       finish();
+//     }
+//   });
+// }
