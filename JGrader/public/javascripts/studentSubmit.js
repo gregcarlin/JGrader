facfc0b5d75838d9023779ad20980657a7f1c423
Brian <brian@convolve.com>
2014-12-11 21:49:34 -0500

    another attempt

--- JGrader/public/javascripts/studentSubmit.js
+++ JGrader/public/javascripts/studentSubmit.js
@@ -1,38 +1,82 @@
-+ function($) {
+// + function($) {
+//   'use strict';
+//
+//   // UPLOAD CLASS DEFINITION
+//   // ======================
+//
+//   var dropZone = document.getElementById('drop-zone');
+//   var uploadForm = document.getElementById('js-upload-form');
+//
+//   var startUpload = function(files) {
+//     console.log(files)
+//   }
+//
+//   uploadForm.addEventListener('submit', function(e) {
+//     var uploadFiles = document.getElementById('js-upload-files').files;
+//     e.preventDefault()
+//
+//     startUpload(uploadFiles)
+//   })
+//
+//   dropZone.ondrop = function(e) {
+//     e.preventDefault();
+//     this.className = 'upload-drop-zone';
+//
+//     startUpload(e.dataTransfer.files)
+//   }
+//
+//   dropZone.ondragover = function() {
+//     this.className = 'upload-drop-zone drop';
+//     return false;
+//   }
+//
+//   dropZone.ondragleave = function() {
+//     this.className = 'upload-drop-zone';
+//     return false;
+//   }
+//
+// }(jQuery);
+require(['html5Upload'], function (html5Upload) {
   'use strict';
+  html5Upload.initialize({
+    // URL that handles uploaded files
+    uploadUrl: '/assignment/:id/submit',
 
-  // UPLOAD CLASS DEFINITION
-  // ======================
+    // HTML element on which files should be dropped (optional)
+    dropContainer: document.getElementById('drop-zone'),
 
-  var dropZone = document.getElementById('drop-zone');
-  var uploadForm = document.getElementById('js-upload-form');
+    // HTML file input element that allows to select files (optional)
+    inputField: document.getElementById('file-selection'),
 
-  var startUpload = function(files) {
-    console.log(files)
-  }
+    // Key for the file data (optional, default: 'file')
+    key: 'File',
 
-  uploadForm.addEventListener('submit', function(e) {
-    var uploadFiles = document.getElementById('js-upload-files').files;
-    e.preventDefault()
+    // Additional data submitted with file (optional)
+    //data: { ProjectId: 1, ProjectName: 'Demo' },
 
-    startUpload(uploadFiles)
-  })
+    // Maximum number of simultaneous uploads
+    // Other uploads will be added to uploads queue (optional)
+    maxSimultaneousUploads: 2,
 
-  dropZone.ondrop = function(e) {
-    e.preventDefault();
-    this.className = 'upload-drop-zone';
+    // Callback for each dropped or selected file
+    // It receives one argument, add callbacks
+    // by passing events map object: file.on({ ... })
+    onFileAdded: function (file) {
 
-    startUpload(e.dataTransfer.files)
-  }
+      var fileModel = new models.FileViewModel(file);
+      uploadsModel.uploads.push(fileModel);
 
-  dropZone.ondragover = function() {
-    this.className = 'upload-drop-zone drop';
-    return false;
-  }
-
-  dropZone.ondragleave = function() {
-    this.className = 'upload-drop-zone';
-    return false;
-  }
-
-}(jQuery);
+      file.on({
+        // Called after received response from the server
+        onCompleted: function (response) {
+          fileModel.uploadCompleted(true);
+        },
+        // Called during upload progress, first parameter
+        // is decimal value from 0 to 100.
+        onProgress: function (progress, fileSize, uploadedBytes) {
+          fileModel.uploadProgress(parseInt(progress, 10));
+        }
+      });
+    }
+  });
+});
