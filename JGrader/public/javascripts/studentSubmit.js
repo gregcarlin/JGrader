72d8b7ae94f6ba12ced90cc571ca4c059468214e
Brian <brian@convolve.com>
2014-12-12 07:44:35 -0500

    comments

--- JGrader/public/javascripts/studentSubmit.js
+++ JGrader/public/javascripts/studentSubmit.js
@@ -36,47 +36,49 @@
 //   }
 //
 // }(jQuery);
-require(['html5Upload'], function (html5Upload) {
-  'use strict';
-  html5Upload.initialize({
-    // URL that handles uploaded files
-    uploadUrl: '/assignment/:id/submit',
 
-    // HTML element on which files should be dropped (optional)
-    dropContainer: document.getElementById('drop-zone'),
-
-    // HTML file input element that allows to select files (optional)
-    inputField: document.getElementById('file-selection'),
-
-    // Key for the file data (optional, default: 'file')
-    key: 'File',
-
-    // Additional data submitted with file (optional)
-    //data: { ProjectId: 1, ProjectName: 'Demo' },
-
-    // Maximum number of simultaneous uploads
-    // Other uploads will be added to uploads queue (optional)
-    maxSimultaneousUploads: 2,
-
-    // Callback for each dropped or selected file
-    // It receives one argument, add callbacks
-    // by passing events map object: file.on({ ... })
-    onFileAdded: function (file) {
-
-      var fileModel = new models.FileViewModel(file);
-      uploadsModel.uploads.push(fileModel);
-
-      file.on({
-        // Called after received response from the server
-        onCompleted: function (response) {
-          fileModel.uploadCompleted(true);
-        },
-        // Called during upload progress, first parameter
-        // is decimal value from 0 to 100.
-        onProgress: function (progress, fileSize, uploadedBytes) {
-          fileModel.uploadProgress(parseInt(progress, 10));
-        }
-      });
-    }
-  });
-});
+//
+// require(['html5Upload'], function (html5Upload) {
+//   'use strict';
+//   html5Upload.initialize({
+//     // URL that handles uploaded files
+//     uploadUrl: '/assignment/:id/submit',
+//
+//     // HTML element on which files should be dropped (optional)
+//     dropContainer: document.getElementById('drop-zone'),
+//
+//     // HTML file input element that allows to select files (optional)
+//     inputField: document.getElementById('file-selection'),
+//
+//     // Key for the file data (optional, default: 'file')
+//     key: 'File',
+//
+//     // Additional data submitted with file (optional)
+//     //data: { ProjectId: 1, ProjectName: 'Demo' },
+//
+//     // Maximum number of simultaneous uploads
+//     // Other uploads will be added to uploads queue (optional)
+//     maxSimultaneousUploads: 2,
+//
+//     // Callback for each dropped or selected file
+//     // It receives one argument, add callbacks
+//     // by passing events map object: file.on({ ... })
+//     onFileAdded: function (file) {
+//
+//       var fileModel = new models.FileViewModel(file);
+//       uploadsModel.uploads.push(fileModel);
+//
+//       file.on({
+//         // Called after received response from the server
+//         onCompleted: function (response) {
+//           fileModel.uploadCompleted(true);
+//         },
+//         // Called during upload progress, first parameter
+//         // is decimal value from 0 to 100.
+//         onProgress: function (progress, fileSize, uploadedBytes) {
+//           fileModel.uploadProgress(parseInt(progress, 10));
+//         }
+//       });
+//     }
+//   });
+// });
