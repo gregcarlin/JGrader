a9af316b630fa58e97b1a9a6150e80915e73a08b
Greg Carlin <gregthegeek@optonline.net>
2014-12-13 21:52:48 -0500

    Teachers can now edit grades
    Just not in the database

--- JGrader/public/javascripts/submission.js
+++ JGrader/public/javascripts/submission.js
@@ -4,3 +4,25 @@ $('#input-expand').click(function() {
   inputExpand.toggleClass('glyphicon-collapse-down');
   $('#input-text').toggle();
 });
+
+$('#edit-grade').click(function() {
+  $('#edit-grade').hide();
+  $('#text').hide();
+  var text = $('#text').html();
+  if(text != 'Not graded.') {
+    $('#edit').val(text);
+  }
+  $('#edit').show();
+  $('#edit').focus();
+});
+
+$('#edit').blur(function() {
+  $('#edit').hide();
+  var text = $('#edit').val();
+  if(text.length > 0) {
+    $('#text').html(text);
+    // todo send update to database
+  }
+  $('#text').show();
+  $('#edit-grade').show();
+});
