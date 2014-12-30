9dadb276793fa737c63555c3cf207a2afd029159
Brian <brian@convolve.com>
2014-12-30 14:48:02 -0500

    on upload compilation finished

--- JGrader/app.js
+++ JGrader/app.js
@@ -23,7 +23,13 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer({inMemory: true}));
+app.use(multer({
+  inMemory: false,
+  dest: './uploads/',
+  rename: function(fieldname,filename) {
+    return filename;
+  }
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
