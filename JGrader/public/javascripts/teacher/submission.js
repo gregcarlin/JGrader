15c514b44a9f904d3275c276ba0db7073aac3fe6
Greg Carlin <gregthegeek@optonline.net>
2015-01-01 13:42:31 -0500

    Generalized editing framework
    Should be pretty easy to add editing of small things now using edit.js,
    will finish implementing this for assignment page for #8

--- JGrader/public/javascripts/teacher/submission.js
+++ JGrader/public/javascripts/teacher/submission.js
@@ -5,43 +5,6 @@ $('#input-expand').click(function() {
   $('#input-text').toggle();
 });
 
-$('#edit-grade').click(function() {
-  $('#edit-grade').hide();
-  $('#text').hide();
-  var text = $('#text').html();
-  if(text != 'Not graded.') {
-    $('#edit').val(text);
-  }
-  $('#edit').show();
-  $('#edit').focus();
-});
-
-$('#edit').blur(function() {
-  $('#edit').hide();
-  var text = $('#edit').val();
-  if(text.length > 0) {
-    $('#text').html(text);
-    var url = document.URL;
-    if(url.charAt(url.length-1) != '/') url += '/';
-    $.post(url + 'updategrade/' + text, '', function(data, textStatus, jqXHR) {
-      var code = jqXHR.responseText;
-      if(code == 0) {
-        // success, do nothing
-      } else if(code == 1) {
-        alert('That is not a valid grade.');
-      } else {
-        alert('An error has occurred, please reload the page and try again.');
-      }
-    });
-  }
-  $('#text').show();
-  $('#edit-grade').show();
-});
-
-$('#edit').keyup(function(event) {
-  if(event.which == 13) $('#edit').blur();
-});
-
 $('#execute').click(function() {
   $('#output-text').html('<span class="glyphicon glyphicon-refresh"></span>'); // todo make this spin so it actually looks like a loading thing
   var fileID = $('.tab-content .active').attr('id');
