fa3c01e89e619b59467f954d2d997a211c70173d
Greg Carlin <gregthegeek@optonline.net>
2015-01-02 12:33:40 -0500

    Adding error support to student section

--- JGrader/routes/student.js
+++ JGrader/routes/student.js
@@ -2,7 +2,6 @@ require('./common');
 var router = express.Router();
 var multer  = require('multer');
 var moment = require('moment');
-var child;
 
 var render = function(page, options, res) {
   switch(page) {
@@ -10,17 +9,20 @@ var render = function(page, options, res) {
       break;
     case 'assignmentList':
       options.page = 1;
-      options.title = 'Assignments';
+      options.title = 'Your Assignments';
       break;
     case 'assignment':
       options.page = 1;
+      // title should already be set
       break;
     case 'sectionList':
       options.page = 0;
+      options.title = 'Your Sections';
       options.js = ['tooltip'];
       break;
     case 'section':
       options.page = 0;
+      // title should already be set
       break;
     case 'joinSection':
       options.page = 0;
@@ -44,11 +46,11 @@ router.get('/assignment', function(req, res) {
                       AND `enrollment`.`section_id` = `assignments`.`section_id` \
                       AND `sections`.`id` = `enrollment`.`section_id` \
                       AND `sections`.`teacher_id`=`teachers`.`id`", [id], function(err, rows) {
-      // todo: Need to handle errors
       if(err) {
-        res.redirect('/student/section');
+        render('assignmentList', {rows: [], error: 'An unexpected error has occurred.'}, res);
+        if(debug) throw err;
       } else {
-        render('assignmentList', { rows: rows}, res);
+        render('assignmentList', {rows: rows}, res);
       }
     });
   });
@@ -65,28 +67,30 @@ router.get('/assignment/:id', function(req, res) {
                         AND `sections`.`id` = `assignments`.`section_id` \
                         AND `enrollment`.`student_id` = ? \
                         AND `assignments`.`id` = ?", [id, assignmentID], function(err, rows) {
-
-      // todo: Need to handle errors
-      if(err) {
-        res.redirect('/student/assignment');
-      } else {
-        connection.query("SELECT `files`.`name`, `files`.`contents` \
-                          FROM `files`, `students`, `assignments`, `submissions` \
-                          WHERE `submissions`.`assignment_id` = `assignments`.`id` \
-                          AND `submissions`.`student_id` = `students`.`id` \
-                          AND `files`.`submission_id`= `submissions`.`id` \
-                          AND  `students`.`id` = ? AND `assignments`.`id` = ?",[id, assignmentID],function(err, fileData){
-          if(err) {
-            res.redirect('/student/assignment');
-          } else if(fileData.length == 0) {
-            render('assignment', { rows: rows, js: ['student/dropzone', 'student/studentSubmit'] }, res);
-          } else {
-            // Sends file data
-            render('assignment', { rows: rows, fileData: fileData, js: ['prettify'] }, res);
-          }
-        });
-      }
-    });
+        if(err) {
+          render('notFound', {page: 1, type: 'assignment', error: 'An unexpected error has occurred.'}, res);
+          if(debug) throw err;
+        } else if(rows.length <= 0) {
+          render('notFound', {page: 1, type: 'assignment'}, res);
+        } else {
+          connection.query("SELECT `files`.`name`, `files`.`contents` \
+                            FROM `files`, `students`, `assignments`, `submissions` \
+                            WHERE `submissions`.`assignment_id` = `assignments`.`id` \
+                            AND `submissions`.`student_id` = `students`.`id` \
+                            AND `files`.`submission_id`= `submissions`.`id` \
+                            AND  `students`.`id` = ? AND `assignments`.`id` = ?", [id, assignmentID], function(err, fileData){
+            if(err) {
+              render('notFound', {page: 1, type: 'assignment', error: 'An unexpected error has occurred.'}, res);
+              if(debug) throw err;
+            } else if(fileData.length == 0) {
+              render('assignment', { rows: rows, js: ['student/dropzone', 'student/studentSubmit'] }, res);
+            } else {
+              // Sends file data
+              render('assignment', { rows: rows, fileData: fileData, js: ['prettify'] }, res);
+            }
+          });
+        }
+      });
     }
   });
 });
@@ -95,33 +99,33 @@ router.get('/assignment/:id', function(req, res) {
 router.post('/assignment/:id/submit', function(req, res) {
   authStudent(req.cookies.hash, res, function(id) {
     var assignmentID = req.params.id;
-    //console.log(req.files);
     if(req.files){
       connection.query("SELECT `submissions`.`id` \
                         FROM `submissions` \
                         WHERE `submissions`.`student_id` = ? \
                         AND `submissions`.`assignment_id` = ?", [id, req.params.id], function(err, rows) {
-        if(rows.length==0){
+        if(rows.length == 0){
           var timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
           connection.query("INSERT INTO `submissions` VALUES(NULL, ?, ?, ?, NULL)", [req.params.id, id, timestamp], function(err, rows) {
-            if(err){
-              // Need error support
-              res.redirect('/student/assignment');
+            if(err) {
+              render('notFound', {page: 1, type: 'assignment', error: 'An unexpected error has occurred.'}, res);
+              if(debug) throw err;
+            } else {
+              submitFiles(0, req.files, id, assignmentID, function(err) {
+                if(err){
+                  render('notFound', {page: 1, type: 'assignment', error: 'An unexpected error has occurred.'}, res);
+                  if(debug) throw err;
+                } else {
+                  res.send(req.body);
+                }
+              });
             }
-            submitFiles(0, req.files, id, assignmentID, function(err) {
-              if(err){
-                // Need error support
-                res.redirect('/student/assignment');
-              } else {
-                res.send(req.body);
-              }
-            });
           });
         } else {
           submitFiles(0, req.files, id, assignmentID, function(err) {
             if(err) {
-              // Need error support
-              res.redirect('/student/assignment');
+              render('notFound', {page: 1, type: 'assignment', error: 'An unexpected error has occurred.'}, res);
+              if(debug) throw err;
             } else {
               res.send(req.body);
             }
@@ -135,8 +139,8 @@ router.post('/assignment/:id/submit', function(req, res) {
 // Lists all of the current sections (classes)
 router.get('/section', function(req, res) {
   authStudent(req.cookies.hash, res, function(id) {
-    findSectionInfo(id, res, function(rows){
-      render('sectionList', { rows: rows }, res);
+    findSectionInfo(id, res, function(rows) {
+      render('sectionList', {rows: rows}, res);
     });
   });
 });
@@ -144,7 +148,7 @@ router.get('/section', function(req, res) {
 // Asks user for class password
 router.get('/section/joinSection', function(req, res) {
   authStudent(req.cookies.hash, res, function(id) {
-    render('joinSection', {  }, res);
+    render('joinSection', {}, res);
   });
 });
 
@@ -157,10 +161,10 @@ router.get('/section/:id', function(req, res) {
                         WHERE `assignments`.`section_id` = `enrollment`.`section_id` \
                         AND `enrollment`.`student_id` = ? \
                         AND `sections`.`id` = `enrollment`.`section_id` \
-                        AND `enrollment`.`section_id` = ?", [studentID,sectionID], function(err, rows) {
-      // todo: Need to handle errors
+                        AND `enrollment`.`section_id` = ?", [studentID, sectionID], function(err, rows) {
       if(err) {
-        res.redirect('/student/section');
+        render('notFound', {page: 0, type: 'section', error: 'An unexpected error has occurred.'}, res);
+        if(debug) throw err;
       } else {
         render('section', { rows: rows }, res);
       }
@@ -199,7 +203,9 @@ router.post('/section/joinSection', function(req, res) {
 var findSectionInfo = function(id, res, finish) {
   if(id){
     connection.query("SELECT `sections`.`name`,`teachers`.`fname`,`teachers`.`lname`,`sections`.`id` FROM `enrollment`,`sections`,`teachers` WHERE `enrollment`.`section_id` = `sections`.`id` AND `sections`.`teacher_id` = `teachers`.`id` AND `enrollment`.`student_id` = ?", [id], function(err, rows) {
-      if(err || rows.length <= 0) {
+      if(err) {
+        render('notFound', {page: 1, type: 'section', error: 'An unexpected error has occurred.'}, res);
+        if(debug) throw err;
       } else {
         finish(rows);
       }
@@ -220,7 +226,7 @@ var submitFiles = function(i, files, student_id, assignment_id, finish) {
             compileFiles = compileFiles + files[file].path + " ";
           }
           // todo make sure file names aren't something like "&& rm -rf /"
-          child = exec("javac " + compileFiles, function (error, stdout, stderr) {
+          exec("javac " + compileFiles, function (error, stdout, stderr) {
             for(file in files) {
               var compilePath = files[file].path.substr(0, files[file].path.length-4) + "class";
               fs.readFile(files[file].path, function(err, javaData) {
