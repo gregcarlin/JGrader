5f47e9c25a6bc145ae28a3d43294a502c61ba23f
Greg Carlin <gregthegeek@optonline.net>
2015-01-23 16:07:01 -0500

    Updated editing system (edit.js), fixes #28

--- JGrader/public/javascripts/teacher/edit.js
+++ JGrader/public/javascripts/teacher/edit.js
@@ -1,4 +1,16 @@
+// http://stackoverflow.com/questions/646628/how-to-check-if-a-string-startswith-another-string
+if (typeof String.prototype.startsWith != 'function') {
+  String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) == str;
+  };
+}
+
 $('.edit').each(function(index, element) {
+
+  var span = $('span', element);
+  if(span.html().length <= 0) {
+    span.html('<em>' + $(element).attr('data-default') + '</em>');
+  }
 
   // do stuff when edit icon is clicked
   $(element).next().click(function() {
@@ -10,7 +22,7 @@ $('.edit').each(function(index, element) {
     icon.hide();
     span.hide();
     var text = span.html();
-    if(text != 'Not graded.') {
+    if(text != 'Not graded.' && !text.startsWith('<em>')) {
       input.val(text);
     }
     input.width(span.width());
@@ -28,14 +40,14 @@ $('.edit').each(function(index, element) {
     input.hide();
     // todo show loading thing
     var text = input.val();
-    if(text.length > 0) {
+    if(par.attr('data-default') != 'none' || text.length > 0) {
       var url = document.URL;
       if(url.charAt(url.length-1) != '/') url += '/';
       $.post(url + par.attr('data-key') + '/' + text.replace(new RegExp('/', 'g'), '-'), '', function(data, textStatus, jqXHR) {
         var code = data.code;
         if(code == 0) {
           // success, update text
-          span.html(data.newValue);
+          span.html(data.newValue.length > 0 ? data.newValue : '<em>' + par.attr('data-default') + '</em>');
         } else if(code == 1) {
           alert('That is not a valid value.');
         } else {
