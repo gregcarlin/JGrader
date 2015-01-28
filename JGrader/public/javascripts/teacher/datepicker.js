c340a727c5e0bcd01e94bd8fb699300c6fba7e10
Greg Carlin <gregthegeek@optonline.net>
2015-01-27 20:58:12 -0500

    Working on #30
    Remaining bugs:
    Teacher can’t set due time
    Date picker doesn’t start on current due date

--- JGrader/public/javascripts/teacher/datepicker.js
+++ JGrader/public/javascripts/teacher/datepicker.js
@@ -1,1 +1,29 @@
 $('#due').datetimepicker();
+
+var makeEditable = function(text) {
+  text = text.replace('st','').replace('nd','').replace('rd','').replace('th','');
+  if(text.indexOf(', 20') < 0) text += ', 2015';
+  var date = new Date(Date.parse(text));
+  return date.getFullYear() + '/' + (date.getMonth() + 1) + '/' + date.getDate();
+}
+
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+var makeReadable = function(text) {
+  var date = new Date(Date.parse(text));
+  var formatted = months[date.getMonth()] + ' ' + date.getDate();
+  switch(date.getDate()) {
+    case 1:
+      formatted += 'st'; break;
+    case 2:
+      formatted += 'nd'; break;
+    case 3:
+      formatted += 'rd'; break;
+    default:
+      formatted += 'th'; break;
+  }
+  if(date.getYear() != new Date(Date.now()).getYear()) { // if date is not in current year
+    formatted += ', ' + date.getFullYear();
+  }
+  return formatted;
+}
