c782fdd808ee0a76b6bff189540d5b4377834942
Greg Carlin <gregthegeek@optonline.net>
2015-02-13 22:33:05 -0600

    tweaking .gitignore

--- JGrader/routes/teacher.js
+++ JGrader/routes/teacher.js
@@ -511,15 +511,17 @@ router.get('/student', function(req, res) {
                     `students`.`fname`,\
                     `students`.`lname`,\
                     `sections`.`id` AS `sid`,\
-                    `sections`.`name` AS `sname` \
+                    `sections`.`name` AS `sname`,\
+                    COUNT(`submissions`.`id`) \
                   FROM \
-                    `students`,\
+                    (`students` LEFT JOIN `submissions` ON `students`.`id` = `submissions`.`student_id`),\
                     `enrollment`,\
                     `sections` \
                   WHERE \
                     `sections`.`teacher_id` = ? AND \
                     `enrollment`.`section_id` = `sections`.`id` AND \
-                    `enrollment`.`student_id` = `students`.`id`", [req.user.id], function(err, rows) {
+                    `enrollment`.`student_id` = `students`.`id` \
+                  GROUP BY `submissions`.`id`", [req.user.id], function(err, rows) {
     render('studentList', {rows: rows}, res);
   });
 });
