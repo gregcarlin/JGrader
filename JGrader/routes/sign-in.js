04a99b81e8f15e8ce7032ca88065fbe3af59caf2
Greg Carlin <gregthegeek@optonline.net>
2015-02-16 12:31:21 -0600

    Hopefully the solution to #35

--- JGrader/routes/sign-in.js
+++ JGrader/routes/sign-in.js
@@ -1,20 +1,9 @@
 // Created by Brian Singer and Greg Carlin in 2015.
 // Copyright (c) 2015 JGrader. All rights reserved.
 
-var express = require('express');
-var router  = express.Router();
-var creds   = require('./credentials');
+require('./common');
+var router = express.Router();
 var crypto  = require('crypto');
-
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : creds.mysql_host,
-  port     : creds.mysql_port,
-  database : creds.mysql_db,
-  user     : creds.mysql_user,
-  password : creds.mysql_pass
-});
-connection.connect(); // we should probably close this at some point [connection.end()]
 
 /* GET home page. */
 router.get('/', function(req, res) {
