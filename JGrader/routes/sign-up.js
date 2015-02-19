63b8116a126ddf976fd6e864814477d28f145f45
Greg Carlin <gregthegeek@optonline.net>
2015-02-18 23:10:38 -0600

    Are you happy now, Jason?
    I did it with vim, too

--- JGrader/routes/sign-up.js
+++ JGrader/routes/sign-up.js
@@ -11,12 +11,12 @@ router.get('/', function(req, res) {
 });
 
 // checks if a user exists in a given database. calls finish() iff user doesn't exist.
-var exists = function(fname, lname, email, role, db, res, finish) {
-  connection.query("SELECT `id` FROM `" + db + "` WHERE `user` = ?", [email], function(err, rows) {
+var exists = function(user, db, res, finish) {
+  connection.query("SELECT `id` FROM `" + db + "` WHERE `user` = ?", [user.email], function(err, rows) {
     if(err) {
-      res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
+      res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', user: user });
     } else if(rows.length > 0) {
-      res.render('sign-up', { error: 'An account with that email already exists.', fname: fname, lname: lname, email: email, role: role });
+      res.render('sign-up', { error: 'An account with that email already exists.', user: user });
     } else {
       finish();
     }
@@ -24,30 +24,31 @@ var exists = function(fname, lname, email, role, db, res, finish) {
 }
 
 router.post('/', function(req, res) {
-  var fname = req.param('fname');
-  var lname = req.param('lname');
-  var email = req.param('email');
-  var pass  = req.param('password');
-  var role  = req.param('role');
-  if(fname && lname && email && pass && role && (role == 'student' || role == 'teacher' || role == 'assistant')) {
-    exists(fname, lname, email, role, 'students', res, function() {
-      exists(fname, lname, email, role, 'teachers', res, function() {
-        exists(fname, lname, email, role, 'assistants', res, function() {
-          connection.query("INSERT INTO `" + role + "s` VALUES(NULL, ?, AES_ENCRYPT(?, '" + creds.aes_key + "'), ?, ?)", [email, pass, fname, lname], function(err, rows) {
+  var user = {};
+  user.fname = req.param('fname');
+  user.lname = req.param('lname');
+  user.email = req.param('email');
+  user.pass  = req.param('password');
+  user.role  = req.param('role');
+  if(user.fname && user.lname && user.email && user.pass && user.role && (user.role == 'student' || user.role == 'teacher' || user.role == 'assistant')) {
+    exists(user, 'students', res, function() {
+      exists(user, 'teachers', res, function() {
+        exists(user, 'assistants', res, function() {
+          connection.query("INSERT INTO `" + user.role + "s` VALUES(NULL, ?, AES_ENCRYPT(?, '" + creds.aes_key + "'), ?, ?)", [user.email, user.pass, user.fname, user.lname], function(err, rows) {
             if(err) {
-              res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
+              res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', user: user });
             } else {
-              connection.query("SELECT `id` FROM `" + role + "s` WHERE `user` = ?", [email], function(err, rows) {
+              connection.query("SELECT `id` FROM `" + user.role + "s` WHERE `user` = ?", [user.email], function(err, rows) {
                 if(err || rows.length <= 0) {
-                  res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
+                  res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', user: user });
                 } else {
                   var hash = crypto.randomBytes(20).toString('hex'); // http://stackoverflow.com/a/14869745/720889
                   res.cookie('hash', hash);
-                  connection.query('INSERT INTO `sessions-' + role + 's` VALUES(?, ?)', [rows[0].id, hash], function(err, rows) {
+                  connection.query('INSERT INTO `sessions-' + user.role + 's` VALUES(?, ?)', [rows[0].id, hash], function(err, rows) {
                     if(err) {
-                      res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', fname: fname, lname: lname, email: email, role: role });
+                      res.render('sign-up', { error: 'An unknown error has occurred. Please try again later.', user: user });
                     } else {
-                      res.redirect('/' + role);
+                      res.redirect('/' + user.role);
                     }
                   });
                 }
@@ -58,8 +59,9 @@ router.post('/', function(req, res) {
       });
     });
   } else {
-    res.render('sign-up', { error: 'All fields are required.', fname: fname, lname: lname, email: email, role: role });
+    res.render('sign-up', { error: 'All fields are required.', user: user });
   }
 });
 
 module.exports = router;
+
