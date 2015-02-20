0e3f2be614554cef0ccf09ccffa87112c21716a4
Greg Carlin <gregthegeek@optonline.net>
2015-02-20 17:13:48 -0600

    This freakin promise
    Took me about 8 hours to write

--- JGrader/routes/teacher/submission.js
+++ JGrader/routes/teacher/submission.js
@@ -86,54 +86,82 @@ router.post('/:id/updategrade/:grade', function(req, res) {
   });
 });
 
+// variation of fs.mkdir that allows directories to already exist
+var mkdir = function(dir) {
+  return Q.nfcall(fs.mkdir, dir)
+    .catch(function(err) {
+      if(err.code == 'EEXIST') {
+        return;
+      } else {
+        throw err;
+      }
+    });
+};
+
 router.post('/:id/run/:fileIndex', function(req, res) {
   // security to ensure this teacher owns this submission and file
-  connection.query("SELECT \
-                    `files`.`id`,\
-                    `files`.`name`,\
-                    `files`.`compiled` \
-                  FROM `submissions`,`assignments`,`sections`,`files` \
-                  WHERE \
-                    `submissions`.`assignment_id` = `assignments`.`id` AND \
-                    `assignments`.`section_id` = `sections`.`id` AND \
-                    `submissions`.`id` = ? AND \
-                    `sections`.`teacher_id` = ? AND \
-                    `files`.`submission_id` = `submissions`.`id` \
-                  ORDER BY `files`.`id`", [req.params.id, req.user.id], function(err, rows) {
-    if(rows.length <= 0) {
-      res.json({code: 2}); // invalid permissions
-    } else {
-      fs.mkdir('temp/', function(err) {
-        if(err.code != 'EEXIST') throw err;
-
-        for(i in rows) {
-          var name = rows[i].name;
-          rows[i].className = name.substring(0, name.length - 5);
-          // note: working directory seems to be one with app.js in it
-          fs.writeFileSync('temp/' + rows[i].className + '.class', rows[i].compiled);
-        }
-
-        var fileIndex = req.params.fileIndex;
-        if(fileIndex < rows.length) {
-          // note: 'nothing' should refer to an actual policy but it doesn't. referring to something that doesn't exist seems to be the same as referring to a policy that grants nothing.
-          child = exec('cd temp/ && java -Djava.security.manager -Djava.security.policy==nothing ' + rows[req.params.fileIndex].className, {timeout: 10000 /* 10 seconds */}, function(error, stdout, stderr) {
-            for(i in rows) {
-              fs.unlinkSync('temp/' + rows[i].className + '.class');
-            }
-            if(error && error.killed) { // if timeout
-              res.json({code: 0, out: '', err: 'Code took too long to execute! There may be an infinite loop somewhere.'});
-            } else {
-              res.json({code: 0, out: stdout, err: stderr});
-            }
-          });
-          child.stdin.write(req.body.stdin);
-          child.stdin.end(); // forces java process to end at end of stdin (otherwise it would just wait if more input was needed)
-        } else {
-          res.json({code: 1}); // invalid input
-        }
-      });
-    }
-  });
+  query("SELECT \
+           `files`.`id`,\
+           `files`.`name`,\
+           `files`.`compiled` \
+         FROM `submissions`,`assignments`,`sections`,`files` \
+         WHERE \
+           `submissions`.`assignment_id` = `assignments`.`id` AND \
+           `assignments`.`section_id` = `sections`.`id` AND \
+           `submissions`.`id` = ? AND \
+           `sections`.`teacher_id` = ? AND \
+           `files`.`submission_id` = `submissions`.`id` \
+         ORDER BY `files`.`id`", [req.params.id, req.user.id])
+    .then(function(rows) {
+      if(rows[0].length <= 0) {
+        var err = new Error('Invalid permissions');
+        err.jsonCode = 2;
+        throw err;
+      } else {
+        return Q.all([rows, mkdir('temp/')]);
+      }
+    })
+    .then(function(rows) {
+      //console.log('helo world');
+      //console.log(rows[0][0]);
+      rows = rows[0][0];
+      for(i in rows) {
+        var name = rows[i].name;
+        rows[i].className = name.substring(0, name.length - 5);
+        // note: working directory seems to be one with app.js in it
+        fs.writeFileSync('temp/' + rows[i].className + '.class', rows[i].compiled);
+      }
+      var fileIndex = req.params.fileIndex;
+      if(fileIndex < rows.length) {
+        // note: 'nothing' should refer to an actual policy but it doesn't. referring to something that doesn't exist seems to be the same as referring to a policy that grants nothing.
+        var child = Q.nfcall(exec, 'cd temp/ && java -Djava.security.manager -Djava.security.policy==nothing ' + rows[req.params.fileIndex].className, {timeout: 10000 /* 10 seconds */});
+        //child.stdin.write(req.body.stdin);
+        //child.stdin.end(); // forces java process to end at end of stdin (otherwise it would just wait if more input was needed)
+        return Q.all([rows, child]);
+      } else {
+        var err = new Error('Invalid input');
+        err.jsonCode = 1;
+        return err;
+      }
+    })
+    .then(function(test) {
+      var rows = test[0];
+      res.json({code: 0, out: test[1][0], err: test[1][1]});
+      for(i in rows) {
+        fs.unlinkSync('temp/' + rows[i].className + '.class');
+      }
+    })
+    .catch(function(err) {
+      if(err.jsonCode) {
+        res.json({code: err.jsonCode});
+      } else if(err.killed) { // if timeout
+        res.json({code: 0, out: '', err: 'Code took too long to execute! There may be an infinite loop somewhere.'});
+      } else {
+        res.json({code: -1});
+        console.log(err);
+        throw err;
+      }
+    });
 });
 
 module.exports = router;
