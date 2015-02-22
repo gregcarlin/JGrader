7542aaabcd4ea2411119df2d22c3833018eebbbf
Greg Carlin <gregthegeek@optonline.net>
2015-02-22 17:48:32 -0500

    An async

--- JGrader/routes/teacher/assignment.js
+++ JGrader/routes/teacher/assignment.js
@@ -4,6 +4,7 @@
 require('../common');
 var router = express.Router();
 var strftime = require('strftime');
+var async = require('async');
 
 var render = function(page, options, res) {
   options.page = 1;
@@ -35,32 +36,35 @@ var render = function(page, options, res) {
 
 // page that lists assignments
 router.get('/', function(req, res) {
-  run(function* () {
-    try {
-      var result = yield query("SELECT \
-                                  `assignments`.`id` AS `aid`,\
-                                  `assignments`.`name` AS `aname`,\
-                                  `assignments`.`due`,\
-                                  `sections`.`id` AS `sid`,\
-                                  `sections`.`name` AS `sname`,\
-                                  COUNT(DISTINCT(`enrollment`.`student_id`)) AS `total`,\
-                                  COUNT(DISTINCT(`submissions`.`student_id`)) AS `complete`,\
-                                  COUNT(DISTINCT(`submissions`.`grade`)) AS `graded`\
-                                FROM `assignments` \
-                                JOIN `sections` ON `sections`.`id` = `assignments`.`section_id` \
-                                LEFT JOIN `enrollment` ON `sections`.`id` = `enrollment`.`section_id` \
-                                LEFT JOIN `submissions` ON `submissions`.`assignment_id` = `assignments`.`id` \
-                                WHERE `sections`.`teacher_id` = ? \
-                                GROUP BY `assignments`.`id` \
-                                ORDER BY \
-                                  `assignments`.`due` DESC, \
-                                  `assignments`.`name` ASC, \
-                                  `sections`.`name` ASC", [req.user.id]);
-      render('assignmentList', {rows: result}, res);
-    } catch (err) {
-      render('assignmentList', {rows: [], error: 'An unexpected error has occurred.'}, res);
-      throw err;
-    }
+  async.waterfall([
+      function(callback) {
+        connection.query("SELECT \
+                            `assignments`.`id` AS `aid`,\
+                            `assignments`.`name` AS `aname`,\
+                            `assignments`.`due`,\
+                            `sections`.`id` AS `sid`,\
+                            `sections`.`name` AS `sname`,\
+                            COUNT(DISTINCT(`enrollment`.`student_id`)) AS `total`,\
+                            COUNT(DISTINCT(`submissions`.`student_id`)) AS `complete`,\
+                            COUNT(DISTINCT(`submissions`.`grade`)) AS `graded`\
+                          FROM `assignments` \
+                          JOIN `sections` ON `sections`.`id` = `assignments`.`section_id` \
+                          LEFT JOIN `enrollment` ON `sections`.`id` = `enrollment`.`section_id` \
+                          LEFT JOIN `submissions` ON `submissions`.`assignment_id` = `assignments`.`id` \
+                          WHERE `sections`.`teacher_id` = ? \
+                          GROUP BY `assignments`.`id` \
+                          ORDER BY \
+                            `assignments`.`due` DESC, \
+                            `assignments`.`name` ASC, \
+                            `sections`.`name` ASC", [req.user.id], callback);
+      }
+    ], function(err, result) {
+      if(err) {
+        render('assignmentList', {rows: [], error: 'An unexpected error has occurred.'}, res);
+        throw err;
+      } else {
+        render('assignmentList', {rows: result}, res);
+      }
   });
 });
 
