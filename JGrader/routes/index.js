2a2f7f9939989b28d4d1af69638d4fa1c3242189
Greg Carlin <gregthegeek@optonline.net>
2015-02-22 21:13:46 -0500

    hopefully fix auto install

--- JGrader/routes/index.js
+++ JGrader/routes/index.js
@@ -38,7 +38,7 @@ var tryRedirect = function(hash, res, db, finish) {
 }
 
 var gitUpdate = function(req, res) {
-  exec('git pull && npm install', {}, function(error, stdout, stderr) {
+  exec('git pull && npm install && forever restartall', {}, function(error, stdout, stderr) {
     res.json({stdout: stdout, stderr: stderr});
   });
 }
