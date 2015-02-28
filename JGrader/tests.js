5e6c5a735ca56c67f4f7db10a57c1fefc55a6e2d
Greg Carlin <gregthegeek@optonline.net>
2015-02-28 18:18:59 -0500

    More testing

--- JGrader/tests.js
+++ JGrader/tests.js
@@ -1,9 +1,19 @@
-//var page = require('webpage').create();
-
-casper.test.begin('Homepage', 1, function(test) {
-  //page.open('http://127.0.0.1:3000', function(status) {
+casper.test.begin('Homepage', function(test) {
   casper.start('http://127.0.0.1:3000', function() {
     test.assertTitle('jGrader');
+    test.assertExists('input[name="email"]');
+    test.assertExists('input[name="password"]');
+
+    this.fillSelectors('form', {
+      'input[name="email"]' : 'test71@test.com',
+      'input[name="password"]' : 'incorrect'
+    }, true);
+  });
+
+  casper.then(function() {
+    test.assertTitle('Sign In | jGrader');
+    test.assertExists('div.alert-danger');
+    test.assertSelectorHasText('div.alert-danger', 'Incorrect');
   });
 
   casper.run(function() {
