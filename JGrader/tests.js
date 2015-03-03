2ba0954b63acee008c3b5a2ae0ae7763838c9475
Greg Carlin <gregthegeek@optonline.net>
2015-03-02 22:12:54 -0500

    These tests can be pretty satisfying

--- JGrader/tests.js
+++ JGrader/tests.js
@@ -24,20 +24,56 @@ casper.test.begin('Homepage and Failed Sign In', function(test) {
   });
 });
 
+const signIn = function(user, pass, test, page) {
+  test.assertTitle('Sign In | jGrader');
+  test.assertExists('input[name="email"]');
+  test.assertExists('input[name="password"]');
+
+  page.fillSelectors('form', {
+    'input[name="email"]'    : user,
+    'input[name="password"]' : pass
+  }, true);
+};
+
+const signOut = function() {
+  casper.test.begin('Sign Out', function(test) {
+    casper.start(url + 'log-out/', function() {});
+
+    casper.then(function() {
+      test.assertTitle('jGrader');
+    });
+
+    casper.run(function() {
+      test.done();
+    });
+  });
+};
+
 casper.test.begin('Teacher Sign In', function(test) {
   casper.start(url + 'sign-in/', function() {
-    test.assertTitle('Sign In | jGrader');
-    test.assertExists('input[name="email"]');
-    test.assertExists('input[name="password"]');
-
-    this.fillSelectors('form', {
-      'input[name="email"]'    : creds.teacher_user,
-      'input[name="password"]' : creds.teacher_pass
-    }, true);
+    signIn(creds.teacher_user, creds.teacher_pass, test, this);
   });
 
   casper.then(function() {
     test.assertTitle('Your Sections | jGrader');
+    test.assertUrlMatch(/\/teacher\/section/);
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+signOut(); // don't think this is needed with our current set-up. either way, good to test.
+
+casper.test.begin('Student Sign In', function(test) {
+  casper.start(url + 'sign-in/', function() {
+    signIn(creds.student_user, creds.student_pass, test, this);
+  });
+
+  casper.then(function() {
+    test.assertTitle('Your Sections | jGrader');
+    test.assertUrlMatch(/\/student\/section/);
   });
 
   casper.run(function() {
