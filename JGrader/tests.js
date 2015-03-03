6afbd6ac067d448c21dd1f044a7a9f1a5c0aee69
Greg Carlin <gregthegeek@optonline.net>
2015-03-03 18:00:35 -0500

    More testing stuff, minor reorganization

--- JGrader/tests.js
+++ JGrader/tests.js
@@ -1,6 +1,46 @@
 const url = 'http://127.0.0.1:3000/';
 const creds = require('./test_credentials');
 
+// start with some helpful methods for later
+
+// sign in with a given username and password
+const signIn = function(user, pass, test, page) {
+  test.assertTitle('Sign In | jGrader');
+  test.assertExists('input[name="email"]');
+  test.assertExists('input[name="password"]');
+
+  page.fillSelectors('form', {
+    'input[name="email"]'    : user,
+    'input[name="password"]' : pass
+  }, true);
+};
+
+// a test that only asserts the title of the page. name = test name, path = url path (after base url), title = what page title should be
+const testTitle = function(name, path, title) {
+  casper.test.begin(name, function(test) {
+    casper.start(url + path, function() {
+      test.assertTitle(title);
+    });
+
+    casper.run(function() {
+      test.done();
+    });
+  });
+};
+
+// a test that only asserts that the page loaded with a 200 status code. name = test name, path = url path (after base url)
+const testLoad = function(name, path) {
+  casper.test.begin(name, function(test) {
+    casper.start(url + path, function() {
+      test.assertHttpStatus(200);
+    });
+
+    casper.run(function() {
+      test.done();
+    });
+  });
+};
+
 casper.test.begin('Homepage and Failed Sign In', function(test) {
   casper.start(url, function() {
     test.assertTitle('jGrader');
@@ -24,31 +64,6 @@ casper.test.begin('Homepage and Failed Sign In', function(test) {
   });
 });
 
-const signIn = function(user, pass, test, page) {
-  test.assertTitle('Sign In | jGrader');
-  test.assertExists('input[name="email"]');
-  test.assertExists('input[name="password"]');
-
-  page.fillSelectors('form', {
-    'input[name="email"]'    : user,
-    'input[name="password"]' : pass
-  }, true);
-};
-
-const signOut = function() {
-  casper.test.begin('Sign Out', function(test) {
-    casper.start(url + 'log-out/', function() {});
-
-    casper.then(function() {
-      test.assertTitle('jGrader');
-    });
-
-    casper.run(function() {
-      test.done();
-    });
-  });
-};
-
 casper.test.begin('Teacher Sign In', function(test) {
   casper.start(url + 'sign-in/', function() {
     signIn(creds.teacher_user, creds.teacher_pass, test, this);
@@ -64,13 +79,25 @@ casper.test.begin('Teacher Sign In', function(test) {
   });
 });
 
-/*casper.test.begin('Teacher Assignment List', function(test) {
-  casper.start(url + 'teacher/assignment', function() {
-    
-  });
-});*/
+testTitle('Teacher Section List', 'teacher/section', 'Your Sections | jGrader');
+// there is no csv for section list
+testTitle('Teacher Assignment List', 'teacher/assignment', 'Your Assignments | jGrader');
+testLoad('Teacher Assignment List CSV', 'teacher/assignment.csv');
+testTitle('Teacher Student List', 'teacher/student', 'Your Students | jGrader');
+testLoad('Teacher Student List CSV', 'teacher/student.csv');
 
-signOut(); // don't think this is needed with our current set-up. either way, good to test.
+// don't think this is needed with our current set-up. either way, good to test.
+casper.test.begin('Sign Out', function(test) {
+  casper.start(url + 'log-out/', function() {});
+
+  casper.then(function() {
+    test.assertTitle('jGrader');
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
 
 casper.test.begin('Student Sign In', function(test) {
   casper.start(url + 'sign-in/', function() {
@@ -86,4 +113,7 @@ casper.test.begin('Student Sign In', function(test) {
     test.done();
   });
 });
+
+testTitle('Student Section List', 'student/section', 'Your Sections | jGrader');
+testTitle('Student Assignment List', 'student/assignment', 'Your Assignments | jGrader');
 
