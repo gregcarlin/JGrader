116538cb4ca021c4beca2b3b64e79d06562a5ab0
Greg Carlin <gregthegeek@optonline.net>
2015-03-21 18:57:50 -0400

    You can actually create tests now

--- JGrader/public/javascripts/teacher/caseCreate.js
+++ JGrader/public/javascripts/teacher/caseCreate.js
@@ -1,5 +1,8 @@
+var index = 0;
+
 var addRow = function() {
-  $('form#caseCreate div').append('<input type="text" class="form-control input" placeholder="Test Input" /><input type="text" class="form-control output" placeholder="Expected Output" />');
+  $('form#caseCreate div').append('<input type="text" class="form-control input" placeholder="Test Input" name="in_case[]" /><input type="text" class="form-control output" placeholder="Expected Output" name="out_case[]" />');
+  index++;
 };
 
 $(document).ready(function() {
