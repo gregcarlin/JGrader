d41dcedbd307256e5889716fa819345632e279aa
Greg Carlin <gregthegeek@optonline.net>
2015-03-24 17:18:21 -0400

    this is how we should handle errors

--- JGrader/app.js
+++ JGrader/app.js
@@ -57,29 +57,18 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+// error handler
+app.use(function(err, req, res, next) {
+  if(err.handled) {
+    console.error(err);
+    console.error(err.stack);
+  } else {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
       error: err
     });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  }
 });
-
 
 module.exports = app;
