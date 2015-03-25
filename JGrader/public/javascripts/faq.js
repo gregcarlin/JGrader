169b26ef1f9aa69426b4ee4cccab7175f3e72a44
Greg Carlin <gregthegeek@optonline.net>
2015-03-25 09:47:45 -0400

    interactive faq

---
+++ JGrader/public/javascripts/faq.js
@@ -0,0 +1,7 @@
+$('dl.lead .fa').each(function(index) {
+  $(this).click(function() {
+    $($('dl.lead dd').get(index)).toggle();
+    $(this).toggleClass('fa-plus-square');
+    $(this).toggleClass('fa-minus-square');
+  });
+});
