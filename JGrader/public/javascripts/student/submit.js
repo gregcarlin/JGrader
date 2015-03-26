56a79f27a4d176f2bb34ac2162c3896248b4701b
Greg Carlin <gregthegeek@optonline.net>
2015-03-26 18:39:08 -0400

    ok, not bad

--- JGrader/public/javascripts/student/submit.js
+++ JGrader/public/javascripts/student/submit.js
@@ -73,76 +73,157 @@ document.querySelector("#actions .start").onclick = function() {
   myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
 };
 
-// The Browser API key obtained from the Google Developers Console.
-// Replace with your own Browser API key, or your own key.
-var developerKey = 'AIzaSyA-SyAcuSH8rt5FTFng9m668zG41pEImtM';
-
-// The Client ID obtained from the Google Developers Console. Replace with your own Client ID.
-var clientId = '835071763335-v3cfbmm39vboo0lh1dureqe0cqkop6j9.apps.googleusercontent.com';
-
-// Replace with your own App ID. (Its the first number in your Client ID)
-var appId = '835071763335';
-
-// Scope to use to access user's Drive items.
-var scope = ['https://www.googleapis.com/auth/drive.readonly'];
-
-var pickerApiLoaded = false;
-var oauthToken;
-
-// Use the Google API Loader script to load the google.picker script.
-function loadPicker() {
-  gapi.load('auth', {'callback': onAuthApiLoad});
-  gapi.load('picker', {'callback': onPickerApiLoad});
-}
-
-function onAuthApiLoad() {
-  window.gapi.auth.authorize(
-      {
-        'client_id': clientId,
-        'scope': scope,
-        'immediate': false
-      },
-      handleAuthResult);
-}
-
-function onPickerApiLoad() {
-  pickerApiLoaded = true;
-  createPicker();
-}
-
-function handleAuthResult(authResult) {
-  if (authResult && !authResult.error) {
-    oauthToken = authResult.access_token;
-    createPicker();
+/**!
+ * Google Drive File Picker Example
+ * By Daniel Lo Nigro (http://dan.cx/)
+ */
+(function() {
+  /**
+   * Initialise a Google Driver file picker
+   */
+  var FilePicker = window.FilePicker = function(options) {
+    // Config
+    this.apiKey = options.apiKey;
+    this.clientId = options.clientId;
+ 
+    // Elements
+    this.buttonEl = options.buttonEl;
+ 
+    // Events
+    this.onSelect = options.onSelect;
+    this.buttonEl.addEventListener('click', this.open.bind(this));
+ 
+    // Disable the button until the API loads, as it won't work properly until then.
+    this.buttonEl.disabled = true;
+ 
+    // Load the drive API
+    gapi.client.setApiKey(this.apiKey);
+    gapi.client.load('drive', 'v2', this._driveApiLoaded.bind(this));
+    google.load('picker', '1', { callback: this._pickerApiLoaded.bind(this) });
   }
-}
-
-// Create and render a Picker object for searching files.
-function createPicker() {
-  if (pickerApiLoaded && oauthToken) {
-    var view = new google.picker.View(google.picker.ViewId.FOLDERS);
-    var picker = new google.picker.PickerBuilder()
+ 
+  FilePicker.prototype = {
+    /**
+     * Open the file picker.
+     */
+    open: function() {
+      // Check if the user has already authenticated
+      var token = gapi.auth.getToken();
+      if (token) {
+        this._showPicker();
+      } else {
+        // The user has not yet authenticated with Google
+        // We need to do the authentication before displaying the Drive picker.
+        this._doAuth(false, function() { this._showPicker(); }.bind(this));
+      }
+    },
+ 
+    /**
+     * Show the file picker once authentication has been done.
+     * @private
+     */
+    _showPicker: function() {
+      var accessToken = gapi.auth.getToken().access_token;
+      var view = new google.picker.DocsView();
+      view.setIncludeFolders(true);
+      this.picker = new google.picker.PickerBuilder()
         .enableFeature(google.picker.Feature.NAV_HIDDEN)
         .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
-        .setAppId(appId)
-        .setOAuthToken(oauthToken)
+        .setAppId(this.clientId)
+        .setDeveloperKey(this.apiKey)
+        .setOAuthToken(accessToken)
         .addView(view)
-        .setDeveloperKey(developerKey)
-        .setCallback(pickerCallback)
-        .build();
-      picker.setVisible(true);
+        .setCallback(this._pickerCallback.bind(this))
+        .build()
+        .setVisible(true);
+    },
+ 
+    /**
+     * Called when a file has been selected in the Google Drive file picker.
+     * @private
+     */
+    _pickerCallback: function(data) {
+      if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
+        var file = data[google.picker.Response.DOCUMENTS][0],
+          id = file[google.picker.Document.ID],
+          request = gapi.client.drive.files.get({
+            fileId: id
+          });
+ 
+        request.execute(this._fileGetCallback.bind(this));
+      }
+    },
+    /**
+     * Called when file details have been retrieved from Google Drive.
+     * @private
+     */
+    _fileGetCallback: function(file) {
+      if (this.onSelect) {
+        this.onSelect(file);
+      }
+    },
+ 
+    /**
+     * Called when the Google Drive file picker API has finished loading.
+     * @private
+     */
+    _pickerApiLoaded: function() {
+      this.buttonEl.disabled = false;
+    },
+ 
+    /**
+     * Called when the Google Drive API has finished loading.
+     * @private
+     */
+    _driveApiLoaded: function() {
+      this._doAuth(true);
+    },
+ 
+    /**
+     * Authenticate with Google Drive via the Google JavaScript API.
+     * @private
+     */
+    _doAuth: function(immediate, callback) {
+      gapi.auth.authorize({
+        client_id: this.clientId,
+        scope: 'https://www.googleapis.com/auth/drive.readonly',
+        immediate: immediate
+      }, callback);
+    }
+  };
+}());
+
+function downloadFile(file, callback) {
+  if (file.downloadUrl) {
+    var accessToken = gapi.auth.getToken().access_token;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', file.downloadUrl);
+    xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+    xhr.onload = function() {
+      callback(xhr.responseText);
+    };
+    xhr.onerror = function() {
+      callback(null);
+    };
+    xhr.send();
+  } else {
+    callback(null);
   }
 }
 
-// A simple callback implementation.
-function pickerCallback(data) {
-  if (data.action == google.picker.Action.PICKED) {
-    var fileId = data.docs[0].id;
-    console.log('The user selected: ' + fileId);
-    $.get('https://www.googleapis.com/drive/v2/files/' + fileId + '?key=' + developerKey + '&acknowledgeAbuse=true&alt=media', {}, function(data, textStatus, jqXHR) {
-      // TODO
-      console.log('DATA RECEIVED');
-      console.log(data);
-    });
-  }
+function initPicker() {
+  var picker = new FilePicker({
+    apiKey: 'AIzaSyA-SyAcuSH8rt5FTFng9m668zG41pEImtM',
+    clientId: '835071763335-v3cfbmm39vboo0lh1dureqe0cqkop6j9.apps.googleusercontent.com',
+    buttonEl: document.getElementById('pick'),
+    onSelect: function(file) {
+      downloadFile(file, function(contents) {
+        if(contents) {
+          // TODO
+        } else {
+          // TODO unable to get contents, probably a spreadsheet or something gross
+        }
+      });
+    }
+  });
 }
