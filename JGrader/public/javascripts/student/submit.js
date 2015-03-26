d8aa84a062f601e740d21ced1fb6a0bdd1d5f4b8
Greg Carlin <gregthegeek@optonline.net>
2015-03-26 10:16:09 -0400

    fixed indentation

--- JGrader/public/javascripts/student/submit.js
+++ JGrader/public/javascripts/student/submit.js
@@ -106,40 +106,40 @@ window.gapi.auth.authorize(
 }
 
 function onPickerApiLoad() {
-pickerApiLoaded = true;
-createPicker();
+  pickerApiLoaded = true;
+  createPicker();
 }
 
 function handleAuthResult(authResult) {
-if (authResult && !authResult.error) {
-  oauthToken = authResult.access_token;
-  createPicker();
-}
+  if (authResult && !authResult.error) {
+    oauthToken = authResult.access_token;
+    createPicker();
+  }
 }
 
 // Create and render a Picker object for searching images.
 function createPicker() {
-if (pickerApiLoaded && oauthToken) {
-  var view = new google.picker.View(google.picker.ViewId.DOCS);
-  view.setMimeTypes("image/png,image/jpeg,image/jpg");
-  var picker = new google.picker.PickerBuilder()
-      .enableFeature(google.picker.Feature.NAV_HIDDEN)
-      .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
-      .setAppId(appId)
-      .setOAuthToken(oauthToken)
-      .addView(view)
-      .addView(new google.picker.DocsUploadView())
-      .setDeveloperKey(developerKey)
-      .setCallback(pickerCallback)
-      .build();
-    picker.setVisible(true);
-}
+  if (pickerApiLoaded && oauthToken) {
+    var view = new google.picker.View(google.picker.ViewId.DOCS);
+    view.setMimeTypes("image/png,image/jpeg,image/jpg");
+    var picker = new google.picker.PickerBuilder()
+        .enableFeature(google.picker.Feature.NAV_HIDDEN)
+        .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+        .setAppId(appId)
+        .setOAuthToken(oauthToken)
+        .addView(view)
+        .addView(new google.picker.DocsUploadView())
+        .setDeveloperKey(developerKey)
+        .setCallback(pickerCallback)
+        .build();
+      picker.setVisible(true);
+  }
 }
 
 // A simple callback implementation.
 function pickerCallback(data) {
-if (data.action == google.picker.Action.PICKED) {
-  var fileId = data.docs[0].id;
-  alert('The user selected: ' + fileId);
-}
+  if (data.action == google.picker.Action.PICKED) {
+    var fileId = data.docs[0].id;
+    alert('The user selected: ' + fileId);
+  }
 }
