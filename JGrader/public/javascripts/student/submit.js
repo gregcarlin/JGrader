98315d1b739429e78d57316843f7a2b4cbfcf2ed
Greg Carlin <gregthegeek@optonline.net>
2015-03-26 09:58:16 -0400

    working on gdrive upload

--- JGrader/public/javascripts/student/submit.js
+++ JGrader/public/javascripts/student/submit.js
@@ -76,3 +76,67 @@ document.querySelector("#actions .start").onclick = function() {
   myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
 };
 
+// The Browser API key obtained from the Google Developers Console.
+var developerKey = 'AIzaSyA-SyAcuSH8rt5FTFng9m668zG41pEImtM';
+
+// The Client ID obtained from the Google Developers Console. Replace with your own Client ID.
+var clientId = '835071763335-v3cfbmm39vboo0lh1dureqe0cqkop6j9.apps.googleusercontent.com';
+
+// Scope to use to access user's photos.
+var scope = ['https://www.googleapis.com/auth/photos'];
+
+var pickerApiLoaded = false;
+var oauthToken;
+
+// Use the API Loader script to load google.picker and gapi.auth.
+function onApiLoad() {
+  gapi.load('auth', {'callback': onAuthApiLoad});
+  gapi.load('picker', {'callback': onPickerApiLoad});
+}
+
+function onAuthApiLoad() {
+  window.gapi.auth.authorize(
+      {
+        'client_id': clientId,
+        'scope': scope,
+        'immediate': false
+      },
+      handleAuthResult);
+}
+
+function onPickerApiLoad() {
+  pickerApiLoaded = true;
+  createPicker();
+}
+
+function handleAuthResult(authResult) {
+  if (authResult && !authResult.error) {
+    oauthToken = authResult.access_token;
+    createPicker();
+  }
+}
+
+// Create and render a Picker object for picking user Photos.
+function createPicker() {
+  if (pickerApiLoaded && oauthToken) {
+    var picker = new google.picker.PickerBuilder().
+        addView(google.picker.ViewId.PHOTOS).
+        setOAuthToken(oauthToken).
+        setDeveloperKey(developerKey).
+        setCallback(pickerCallback).
+        build();
+    picker.setVisible(true);
+  }
+}
+
+// A simple callback implementation.
+function pickerCallback(data) {
+  var url = 'nothing';
+  if (data[google.picker.Response.ACTION] == google.picker.Action.PICKED) {
+    var doc = data[google.picker.Response.DOCUMENTS][0];
+    url = doc[google.picker.Document.URL];
+  }
+  var message = 'You picked: ' + url;
+  alert(message);
+}
+
