20cf4a08c2de48e12581b13df37a4575c9b6f083
Brian <brian@convolve.com>
2015-03-29 18:59:52 -0400

    another test

--- JGrader/app.js
+++ JGrader/app.js
@@ -1,7 +1,6 @@
 // Created by Brian Singer and Greg Carlin in 2015.
 // Copyright (c) 2015 JGrader. All rights reserved.
 
-// Test Commit Ignore
 var express      = require('express');
 var path         = require('path');
 var favicon      = require('serve-favicon');
