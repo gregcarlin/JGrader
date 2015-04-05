9dd08934e4a320be3559eeb2f413c96b63b1728e
Greg Carlin <gregthegeek@optonline.net>
2015-04-05 17:09:33 -0400

    working on feedback page

--- JGrader/routes/student.js
+++ JGrader/routes/student.js
@@ -18,7 +18,6 @@ var render = function(page, options, res) {
       break;
     case 'feedback':
       options.title = 'Feedback';
-      options.css = ['feedback'];
       break;
   }
   page = '../' + page;
