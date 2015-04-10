5b1074ef3192c898eb469383966e73d7e5433be7
Greg Carlin <gregthegeek@optonline.net>
2015-04-10 10:43:47 -0400

    resubmit confirmation

--- JGrader/public/javascripts/student/submitted.js
+++ JGrader/public/javascripts/student/submitted.js
@@ -1,5 +1,14 @@
 // Created by Brian Singer and Greg Carlin in 2015.
 // Copyright (c) 2015 JGrader. All rights reserved.
 
-$("#resubmit").attr("href", document.URL + "/resubmit");
+$(document).ready(function() {
+  $('#resubmit').click(function() {
+    $('.modal').modal({}); // show modal
+    $('.modal-dialog').css('z-index', '1500'); // move above backdrop
+  });
+
+  $('#myModal .btn-danger').click(function() {
+    document.location = document.URL + '/resubmit';
+  });
+});
 
