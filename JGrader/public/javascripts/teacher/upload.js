2ec7e088970f12c56c1774a680bf13825b815259
Greg Carlin <gregthegeek@optonline.net>
2015-04-21 19:06:07 -0400

    forgot this

---
+++ JGrader/public/javascripts/teacher/upload.js
@@ -0,0 +1,9 @@
+// Created by Brian Singer and Greg Carlin in 2015.
+// Copyright (c) 2015 JGrader. All rights reserved.
+
+// "myAwesomeDropzone" is the camelized version of the HTML element's ID
+Dropzone.options.assignCreate = {
+  clickable: '.clickable',
+  createImageThumbnails: false,
+  previewsContainer: '.files'
+};
