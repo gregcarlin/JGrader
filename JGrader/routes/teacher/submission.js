c7d51e233939f3493c555f0f0b690cfcfcbafe9a
Greg Carlin <gregthegeek@optonline.net>
2015-04-25 23:10:06 -0400

    differentiate teacher uploaded files for teachers

--- JGrader/routes/teacher/submission.js
+++ JGrader/routes/teacher/submission.js
@@ -55,16 +55,28 @@ router.get('/:id', function(req, res, next) {
     } else {
       connection.query("SELECT `id`,`name`,`contents`,`compiled`,`mime` FROM `files` WHERE `submission_id` = ? ORDER BY `id`", [req.params.id], function(err, fileData) {
         if(err) {
-          render('submission', {title: students[0].fname + ' ' + students[0].lname + "'s submission to " + req.assignment.name, student: students[0], fileData: [], submission: req.submission, assignment: req.assignment, error: 'Unable to retrieve file data.', anyCompiled: true}, res);
+          render('submission', {title: students[0].fname + ' ' + students[0].lname + "'s submission to " + req.assignment.name, student: students[0], fileData: [], submission: req.submission, assignment: req.assignment, error: 'Unable to retrieve file data.', anyCompiled: true, teacherFiles: []}, res);
           err.handled = true;
           next(err);
         } else {
-          var anyCompiled = false;
-          for(file in fileData) {
-            fileData[file].display = isAscii(fileData[file].mime);
-            if(fileData[file].compiled) anyCompiled = true;
-          }
-          render('submission', {title: students[0].fname + ' ' + students[0].lname + "'s submission to " + students[0].name, student: students[0], fileData: fileData, submission: req.submission, assignment: req.assignment, anyCompiled: anyCompiled}, res);
+          connection.query("SELECT `name` FROM `files-teachers` WHERE `assignment_id` = ?", [req.assignment.id], function(err, teacherFiles) {
+            if(err) {
+              render('submission', {title: students[0].fname + ' ' + students[0].lname + "'s submission to " + req.assignment.name, student: students[0], fileData: [], submission: req.submission, assignment: req.assignment, error: 'Unable to retrieve file data.', anyCompiled: true, teacherFiles: []}, res);
+              err.handled = true;
+              next(err);
+            } else {
+              var anyCompiled = false;
+              for(file in fileData) {
+                fileData[file].display = isAscii(fileData[file].mime);
+                if(fileData[file].compiled) anyCompiled = true;
+              }
+              var teacherNames = [];
+              for(i in teacherFiles) {
+                teacherNames.push(teacherFiles[i].name);
+              }
+              render('submission', {title: students[0].fname + ' ' + students[0].lname + "'s submission to " + students[0].name, student: students[0], fileData: fileData, submission: req.submission, assignment: req.assignment, anyCompiled: anyCompiled, teacherFiles: teacherNames}, res);
+            }
+          });
         }
       });
     }
