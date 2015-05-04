5fe759fa802a787f94dbd0c98bc1c286c2e67eef
Greg Carlin <gregthegeek@optonline.net>
2015-05-03 20:49:27 -0400

    graded count is actually accurate now

--- JGrader/routes/teacher/assignment.js
+++ JGrader/routes/teacher/assignment.js
@@ -44,32 +44,14 @@ var render = function(page, options, res) {
 
 // page that lists assignments
 router.get('/', function(req, res, next) {
-    connection.query("SELECT \
-                        `assignments`.`id` AS `aid`,\
-                        `assignments`.`name` AS `aname`,\
-                        `assignments`.`due`,\
-                        `sections`.`id` AS `sid`,\
-                        `sections`.`name` AS `sname`,\
-                        COUNT(DISTINCT(`enrollment`.`student_id`)) AS `total`,\
-                        COUNT(DISTINCT(`submissions`.`student_id`)) AS `complete`,\
-                        COUNT(DISTINCT(`submissions`.`grade`)) AS `graded`\
-                      FROM `assignments` \
-                      JOIN `sections` ON `sections`.`id` = `assignments`.`section_id` \
-                      LEFT JOIN `enrollment` ON `sections`.`id` = `enrollment`.`section_id` \
-                      LEFT JOIN `submissions` ON `submissions`.`assignment_id` = `assignments`.`id` \
-                      WHERE `sections`.`teacher_id` = ? \
-                      GROUP BY `assignments`.`id` \
-                      ORDER BY \
-                        `assignments`.`due` DESC, \
-                        `assignments`.`name` ASC, \
-                        `sections`.`name` ASC", [req.user.id], function(err, result) {
-      if(err) {
-        render('assignmentList', {rows: [], error: 'An unexpected error has occurred.'}, res);
-        err.handled = true;
-        next(err);
-      } else {
-        render('assignmentList', {rows: result}, res);
-      }
+  connection.query("SELECT `assignments`.`id` AS `aid`,`assignments`.`name` AS `aname`,`assignments`.`due`,`sections`.`id` AS `sid`,`sections`.`name` AS `sname`,COUNT(`enrollment`.`student_id`) AS `total`,`temp0`.`complete`,`temp`.`graded` FROM `assignments` JOIN `sections` ON `sections`.`id` = `assignments`.`section_id` LEFT JOIN `enrollment` ON `sections`.`id` = `enrollment`.`section_id` LEFT JOIN (SELECT `assignment_id`,COUNT(*) AS `complete` FROM `submissions` GROUP BY `assignment_id`) AS `temp0` ON `temp0`.`assignment_id` = `assignments`.`id` LEFT JOIN (SELECT `assignment_id`,COUNT(*) AS `graded` FROM `submissions` WHERE `grade` IS NOT NULL GROUP BY `assignment_id`) AS `temp` ON `temp`.`assignment_id` = `assignments`.`id` WHERE `sections`.`teacher_id` = 68 GROUP BY `assignments`.`id`", function(err, result) {
+    if(err) {
+      render('assignmentList', {rows: [], error: 'An unexpected error has occurred.'}, res);
+      err.handled = true;
+      next(err);
+    } else {
+      render('assignmentList', {rows: result}, res);
+    }
   });
 });
 
