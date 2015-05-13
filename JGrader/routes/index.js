9c26e7ca912aa05fc4f0ccf932c795f41ed29bff
Greg Carlin <gregthegeek@optonline.net>
2015-05-13 11:02:49 -0400

    proper error handling

--- JGrader/routes/index.js
+++ JGrader/routes/index.js
@@ -5,6 +5,17 @@ require('./common');
 var router = express.Router();
 var crypto = require('crypto');
 
+// if hash is set to a valid user in the given db they are redirected to that section, otherwise finish is called.
+var tryRedirect = function(hash, res, db, finish) {
+  logIn(hash, db + 's', function(id) {
+    if(id) {
+      res.redirect('/' + db);
+    } else {
+      finish();
+    }
+  });
+}
+
 /* GET home page. */
 router.get('/', function(req, res) {
   var hash = req.cookies.hash;
@@ -20,7 +31,7 @@ router.get('/', function(req, res) {
 router.get('/log-out', function(req, res) {
   var hash = req.cookies.hash;
   res.clearCookie('hash');
-  // todo only delete from where we need to?
+  // TODO only delete from where we need to?
   connection.query("DELETE FROM `sessions-teachers` WHERE `hash` = ?", [hash]);
   connection.query("DELETE FROM `sessions-students` WHERE `hash` = ?", [hash]);
   connection.query("DELETE FROM `sessions-assistants` WHERE `hash` = ?", [hash]);
@@ -54,53 +65,63 @@ var findEmail = function(email, finish) {
   });
 };
 
-router.post('/forgot', function(req, res) {
+router.post('/forgot', function(req, res, next) {
   if(!req.body.email) {
     res.render('forgot', {error: 'All fields are required.'});
   } else {
     findEmail(req.body.email, function(err, db, id) {
       if(err) {
         res.render('forgot', {error: 'An unknown error has occurred.'});
-        throw err;
+        err.handled = true;
+        next(err);
       } else if(!db || !id) {
         res.render('forgot', {error: 'No account with that address exists.'});
       } else {
         var hash = crypto.randomBytes(20).toString('hex');
         var url = 'http://jgrader.com/forgot/' + db + '/' + hash;
         connection.query("UPDATE `" + db + "` SET `pass_reset_hash` = ? WHERE `id` = ?", [hash, id], function(err, result) {
-          var mailOptions = {
-            from: creds.email_user,
-            to: req.body.email,
-            subject: 'Password Recovery',
-            html: 'Your password reset request has been received. If you would like to reset your password, please go to <a href="' + url + '">' + url + '</a>. If you did not want to reset your password, you can safely ignore this message.'
-          };
-          transporter.sendMail(mailOptions, function(err, info) {
-            if(err) {
-              res.render('forgot', {error: 'Unable to send email.'});
-              throw err;
-            } else {
-              res.render('forgot', {success: 'An email has been sent with further instruction.'});
-            }
-          });
+          if(err) {
+            res.render('forgot', {error: 'An unknown error has occurred.'});
+            err.handled = true;
+            next(err);
+          } else {
+            var mailOptions = {
+              from: creds.email_user,
+              to: req.body.email,
+              subject: 'Password Recovery',
+              html: 'Your password reset request has been received. If you would like to reset your password, please go to <a href="' + url + '">' + url + '</a>. If you did not want to reset your password, you can safely ignore this message.'
+            };
+            transporter.sendMail(mailOptions, function(err, info) {
+              if(err) {
+                res.render('forgot', {error: 'Unable to send email.'});
+                err.handled = true;
+                next(err);
+              } else {
+                res.render('forgot', {success: 'An email has been sent with further instruction.'});
+              }
+            });
+          }
         });
       }
     });
   }
 });
 
-router.get('/forgot/:db/:hash', function(req, res) {
+router.get('/forgot/:db/:hash', function(req, res, next) {
   connection.query("SELECT `id` FROM ?? WHERE `pass_reset_hash` = ?", [req.params.db, req.params.hash], function(err, rows) {
     if(err) {
       res.render('forgot', {error: 'An unknown error has occurred.'});
-      // don't throw err because it might be caused by an incorrect database
-      console.log(err);
+      // err might be caused by an incorrect database
+      err.handled = true;
+      next(err);
     } else if(rows.length <= 0) {
       res.render('forgot', {error: 'Your account could not be found. Please try again.'});
     } else {
       signIn(req.params.db, rows[0].id, res, function(err, result) {
         if(err) {
           res.render('forgot', {error: 'An unknown error has occurred.'});
-          throw err;
+          err.handled = true;
+          next(err);
         } else {
           // user will be automatically redirected to their proper section
           res.redirect('/');
@@ -118,22 +139,17 @@ router.get('/privacy', function(req, res) {
   res.render('privacy');
 });
 
-// if hash is set to a valid user in the given db they are redirected to that section, otherwise finish is called.
-var tryRedirect = function(hash, res, db, finish) {
-  logIn(hash, db + 's', function(id) {
-    if(id) {
-      res.redirect('/' + db);
-    } else {
-      finish();
-    }
-  });
-}
-
-router.post('/git-update', function(req, res) {
+router.post('/git-update', function(req, res, next) {
   var hmac = 'sha1=' + crypto.createHmac('sha1', creds.git_secret).update(req.rawBody).digest('hex');
   if(req.headers['x-hub-signature'] == hmac) {
     exec('git pull', {}, function(error, stdout, stderr) {
-      res.json({stdout: stdout, stderr: stderr});
+      if(error) {
+        res.json(error);
+        error.handled = true;
+        next(error);
+      } else {
+        res.json({stdout: stdout, stderr: stderr});
+      }
     });
   } else {
     res.json({stderr: 'Invalid secret.'});
