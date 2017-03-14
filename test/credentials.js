dfc1249266a007e2ec449a77fb8b850a06ae62de
Greg Carlin <gregcarlin1@gmail.com>
2017-03-14 14:13:47 -0400

    Remove credentials

---
+++ test/credentials.js
@@ -0,0 +1,7 @@
+module.exports = {
+  url          : 'http://127.0.0.1:3000/',
+  teacher_user : 'teacher@testcase.com',
+  teacher_pass : 'test',
+  student_user : 'student@testcase.com',
+  student_pass : 'test'
+};
